a7adcdb09bcf2363fba0967a0637f06e360faed8
lizjack <65046262+lizjack@users.noreply.github.com>
2020-07-28 22:03:02 -0700

    Added Class and Methods
    Also added some code notes to help guide creation.

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,11 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+ class Game {
+//start and end the game
+//handle interactions
+//get a random phrase
+//check for a win
+//remove a life from scoreboard
+ }
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,22 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+ class Phrase {
+    constructor(phrase) {
+        this.phrase = phrase;
+    }
+    addPhraseToDisplay() {
+//adds placeholders to the display when the game starts, each letter
+//is represented by an empty box, one list element per letter
+    }
+    checkLetter() {
+//checks to see if the letter selected by the player
+//matches the letter in the phrase        
+    }
+    showMatchedLetter() {
+/** reveals the letters on the board that match the players selection
+ * select elements hide css class with Show css class */        
+    }
+ }
+
